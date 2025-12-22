5d372083fa3f0a6efcab8f012b809bf1f9a90e32
Mubashar91 <114853105+Mubashar91@users.noreply.github.com>
2025-12-22 23:56:37 +0500

    njnn

--- backend/config/tenantConfig.js
+++ backend/config/tenantConfig.js
@@ -10,4 +10,9 @@ export const tenants = {
         uri: "mongodb+srv://mmubasharshahzad40_db_user:lecX9I03UTMWTnxn@cluster0.9r3kmcz.mongodb.net/?appName=Cluster0",
         dbName: "donva"
     }
+    ,
+     "seo": {
+        uri: "mongodb+srv://mmubasharshahzad40_db_user:lecX9I03UTMWTnxn@cluster0.9r3kmcz.mongodb.net/?appName=Cluster0",
+        dbName: "seo"
+    }
 };
