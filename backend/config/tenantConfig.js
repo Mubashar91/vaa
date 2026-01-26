e1919b4fe8094543366cded50354a534dd48ab8d
Mubashar91 <114853105+Mubashar91@users.noreply.github.com>
2026-01-27 00:00:37 +0500

    ok

--- backend/config/tenantConfig.js
+++ backend/config/tenantConfig.js
@@ -65,5 +65,10 @@ export const tenants = {
         dbName: "Social_media_Account_mangement"
     },
 
+     "socialmediaAccountmangement": {
+        uri: "mongodb+srv://mmubasharshahzad40_db_user:lecX9I03UTMWTnxn@cluster0.9r3kmcz.mongodb.net/?appName=Cluster0",
+        dbName: "socialmediaAccountmangement"
+    },
+
 
 };
