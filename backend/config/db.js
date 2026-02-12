e0c4e5142bf1aed8bc9ff92d91e60e36aa82d62a
Mubashar91 <114853105+Mubashar91@users.noreply.github.com>
2026-02-12 18:14:51 +0500

    ok

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export default async function connectDB(uri) {
   // if (!uri) {
   //   throw new Error('MONGO_URI environment variable is required');
-  // }
+  // }     
 
   // mongoose.set('strictQuery', true);
 
