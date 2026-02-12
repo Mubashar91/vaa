89ac579fa4f47af37d808149a08b748eaa54456a
Mubashar91 <114853105+Mubashar91@users.noreply.github.com>
2026-02-12 17:59:20 +0500

    update

--- backend/config/tenantConfig.js
+++ backend/config/tenantConfig.js
@@ -69,7 +69,7 @@ export const tenants = {
         uri: "mongodb+srv://mmubasharshahzad40_db_user:lecX9I03UTMWTnxn@cluster0.9r3kmcz.mongodb.net/?appName=Cluster0",
         dbName: "socialmediaAccountmangement"
     },
-
+   
        "Don_agency": {
         uri: "mongodb+srv://mmubasharshahzad40_db_user:lecX9I03UTMWTnxn@cluster0.9r3kmcz.mongodb.net/?appName=Cluster0",
         dbName: "Don_agency"
